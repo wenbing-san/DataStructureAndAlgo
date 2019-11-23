09ebac22726be4ff27c5f49a7ec0bf9942b46d4c
wenbing-san <zhangwenbing8917@163.com>
2019-11-23 19:37:48 +0800

    Initial commit

---
+++ src/arrayTest/Array.java
@@ -0,0 +1,13 @@
+package arrayTest;
+
+/**
+ * Created by zhang on 2019/11/23.
+ */
+public class Array {
+
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello World!!");
+    }
+}
